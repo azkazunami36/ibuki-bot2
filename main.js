1ebb8ec10454a782ef8e4e8ae31decb06fea6650
AzkSumWave36 <kazunami36.sum@icloud.com>
2022-12-07 20:04:23 +0900

    肝心のコマンドが使えない問題を修正

--- main.js
+++ main.js
@@ -17,16 +17,26 @@ const { Client,
         partials: [Partials.Channel, Partials.GuildMember, Partials.GuildScheduledEvent, Partials.Message, Partials.Reaction, Partials.ThreadMember, Partials.User],
         intents: [GatewayIntentBits.DirectMessageReactions, GatewayIntentBits.DirectMessageTyping, GatewayIntentBits.DirectMessages, GatewayIntentBits.GuildBans, GatewayIntentBits.GuildEmojisAndStickers, GatewayIntentBits.GuildIntegrations, GatewayIntentBits.GuildInvites, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildMessageTyping, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildPresences, GatewayIntentBits.GuildScheduledEvents, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildWebhooks, GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent]
     });
+
+client.on("ready", () => {
+    console.log("準備ok");
+    client.application.commands.set([
+        new SlashCommandBuilder()
+            .setName("Authenticator")
+            .setDescription("認証ボタンを作成します。")
+    ]);
+});
+
 client.on("messageCreate", message => {
 });
 
 client.on("interactionCreate", interaction => {
     switch (interaction.commandName) {
-        case "facterButtonCreate": {
+        case "AuthenticatorButtonCreate": {
                 const button = new ButtonBuilder()
                     .setLabel("認証！")
                     .setStyle(ButtonStyle.Primary)
-                    .setCustomId("facterButton");
+                    .setCustomId("authenticatorButton");
                 const embed = new EmbedBuilder()
                     .setTitle("認証をして僕たちとこのサーバーを楽しもう！")
                     .setDescription("✅認証は下のボタンを押下する必要があります。")
@@ -35,6 +45,7 @@ client.on("interactionCreate", interaction => {
                     .then(message => {
                         console.log(message);
                         console.log(message.id);
+                        interaction.reply({ content: "作成が完了しました！", ephemeral: true });
                     });
                 break;
             }
