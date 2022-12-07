79a6b1b1b03fea7b4500d032e4af635933a6717a
AzkSumWave36 <kazunami36.sum@icloud.com>
2022-12-08 00:29:29 +0900

    番号を選択して認証が可能になります

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const { RoleSelectMenuBuilder } = require('@discordjs/builders');
+
 require('dotenv').config();
 const { Client,
     GatewayIntentBits,
@@ -21,16 +23,23 @@ const { Client,
     });
 
 client.on(Events.ClientReady, () => {
-    console.log("準備ok");
+    console.info("準備ok");
     client.guilds.cache.map(guild => {
         client.guilds.cache.get(guild.id).commands.set([]);
         client.application.commands.set([
             new SlashCommandBuilder()
                 .setName("authbtncreate")
                 .setDescription("認証ボタンを作成します。")
+                .addRoleOption(option => option
+                    .setName("roles")
+                    .setDescription("付与する際に使用するロールを選択します。")
+                    .setRequired(true)
+                )
         ]);
     });
 });
+
+const json = {};
 
 client.on(Events.MessageCreate, message => {
 });
@@ -38,10 +47,11 @@ client.on(Events.MessageCreate, message => {
 client.on(Events.InteractionCreate, interaction => {
     switch (interaction.commandName) {
         case "authbtncreate": {
+            const roleID = interaction.options.getRole("roles").id;
             const button = new ButtonBuilder()
                 .setLabel("認証！")
                 .setStyle(ButtonStyle.Primary)
-                .setCustomId("authenticatorbutton");
+                .setCustomId("authenticatorbutton" + roleID);
             const components = new ActionRowBuilder()
                 .addComponents(button);
             const embed = new EmbedBuilder()
@@ -49,58 +59,64 @@ client.on(Events.InteractionCreate, interaction => {
                 .setDescription("✅認証は下のボタンを押下する必要があります。")
                 .setAuthor(interaction.guild);
             interaction.channel.send({ embeds: [embed], components: [components] })
-                .then(message => {
-                    console.log(message);
-                    console.log(message.id);
+                .then(() => {
                     interaction.reply({ content: "作成が完了しました！", ephemeral: true });
                 });
             break;
         }
     };
     if (interaction.isButton()) {
-        switch (interaction.customId) {
-            case "authenticatorbutton": {
-                console.log("ok");
-                const embed = new EmbedBuilder();
-                const data = {
-                    calc_type: 0,
-                    one: 0,
-                    two: 0,
-                    ord: []
-                };
-                data.calc_type = Math.floor(Math.random() * 2);
-                data.one = Math.floor(Math.random() * 9);
-                data.two = Math.floor(Math.random() * 9);
-                data.ord.push({ type: "+", Num: data.one + data.two });
-                data.ord.push({ type: "-", Num: data.one - data.two });
-                data.ord.push({ type: "*", Num: data.one * data.two });
-                embed.setTitle("問題！");
-                embed.setDescription("下の計算を解くだけで認証が出来ます！");
-                embed.addFields({
-                    name: data.one + data.ord[data.calc_type].type + data.two + "=",
-                    value: "の答えをしたから選びましょう。"
-                });
-                for (let i = (data.ord.length - 1); i != 0; i--) {
-                    const random = Math.floor(Math.random() * i);
-                    var tmp = data.ord[i];
-                    data.ord[i] = data.ord[random];
-                    data.ord[random] = tmp;
-                };
-                const components = new ActionRowBuilder();
-                for (let i = 0; data.ord.length != i; i++) {
-                    components.addComponents(
-                        new ButtonBuilder()
-                            .setLabel(String(data.ord[i].Num))
-                            .setStyle(ButtonStyle.Primary)
-                            .setCustomId("is" + data.ord[i].Num)
-                    );
-                }
-                interaction.reply({ embeds: [embed], components: [components] });
-                return;
-                const role = interaction.guild.roles.cache.get("1035908195186638909");
-                if (member.roles.cache.has("1035908195186638909")) {
+        if (interaction.customId.match(/authenticatorbutton[0-9]/)) {
+            const roleID = interaction.customId.split("authenticatorbutton")[1];
+            const embed = new EmbedBuilder();
+            json[interaction.user.id] = {
+                calc_type: 0,
+                one: 0,
+                two: 0,
+                ord: []
+            };
+            let data = json[interaction.user.id];
+            data.calc_type = Math.floor(Math.random() * 2);
+            data.one = Math.floor(Math.random() * 9);
+            data.two = Math.floor(Math.random() * 9);
+            data.ord.push({ type: "+", Num: data.one + data.two });
+            data.ord.push({ type: "-", Num: data.one - data.two });
+            data.ord.push({ type: "*", Num: data.one * data.two });
+            for (let i = (data.ord.length - 1); i != 0; i--) {
+                const random = Math.floor(Math.random() * i);
+                var tmp = data.ord[i];
+                data.ord[i] = data.ord[random];
+                data.ord[random] = tmp;
+            };
+            embed.setTitle("問題！");
+            embed.setDescription("下の計算を解くだけで認証が出来ます！");
+            embed.addFields({
+                name: data.one + data.ord[data.calc_type].type + data.two + "=",
+                value: "の答えを下から選びましょう。"
+            });
+            const components = new ActionRowBuilder();
+            for (let i = 0; data.ord.length != i; i++) {
+                components.addComponents(
+                    new ButtonBuilder()
+                        .setLabel(String(data.ord[i].Num))
+                        .setStyle(ButtonStyle.Primary)
+                        .setCustomId(roleID + "calc" + i)
+                );
+            };
+            interaction.reply({
+                embeds: [embed],
+                components: [components],
+                ephemeral: true
+            });
+        } else if (interaction.customId.match(/[0-9]calc[0-9]/)) {
+            const roleID = interaction.customId.split("calc")[0];
+            const request = Number(interaction.customId.split("calc")[1]);
+            if (interaction.customId.match()) {
+                const awnser = json[interaction.user.id].calc_type;
+                if (request == awnser) {
+                    const role = interaction.guild.roles.cache.get(roleID);
                     try {
-                        member.roles.add(role);
+                        interaction.member.roles.add(role);
                         interaction.reply({
                             content: "認証に成功しました！",
                             ephemeral: true
@@ -110,10 +126,15 @@ client.on(Events.InteractionCreate, interaction => {
                             content: "認証でエラーが発生してしまいました...\nエラーは管理者が確認し修正します。",
                             ephemeral: true
                         });
-                    }
-                }
+                    };
+                } else {
+                    interaction.reply({
+                        content: "あぁ...答えが違いますよ...\nもっかいクリックしてやりなおしましょ！",
+                        ephemeral: true
+                    });
+                };
             }
-        }
+        };
     }
 });
 client.login(process.env.token);
